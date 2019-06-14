2690f645f50c0bdb0e23aad1159e82afcdd3e3c6
Lauren ten Hoor <laurentenhoor@gmail.com>
2019-06-14 13:17:33 +0200

    Single quotes prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import { JamBuddy } from "./JamBuddy";
-import Tone from "tone";
-import React from "react";
+import { JamBuddy } from './JamBuddy';
+import Tone from 'tone';
+import React from 'react';
 
 export default class Home extends React.Component {
   private jamBuddy: JamBuddy = new JamBuddy();
   private selected = {
-    root: "A",
-    chordType: "Maj"
+    root: 'A',
+    chordType: 'Maj'
   };
 
   onChangeRoot(event: React.ChangeEvent<HTMLSelectElement>) {
@@ -31,28 +31,28 @@ export default class Home extends React.Component {
       Tone.Master
     );
 
-    polySynth.triggerAttackRelease(["C4", "E4", "G4"], "1n");
+    polySynth.triggerAttackRelease(['C4', 'E4', 'G4'], '1n');
   }
 
   render() {
     return (
       <div>
         <h1>jambuddy</h1>
-        <select name="root" id="root" onChange={e => this.onChangeRoot(e)}>
+        <select name='root' id='root' onChange={e => this.onChangeRoot(e)}>
           {this.jamBuddy.rootArray.map(root => {
             return <option value={root}>{root}</option>;
           })}
         </select>
         <select
-          name="chordType"
-          id="chordType"
+          name='chordType'
+          id='chordType'
           onChange={e => this.onChangeChordType(e)}
         >
           {this.jamBuddy.chordTypeArray.map(chordType => {
             return <option value={chordType}>{chordType}</option>;
           })}
         </select>
-        <button onClick={() => this.playChord()} className="sound-button">
+        <button onClick={() => this.playChord()} className='sound-button'>
           Music please
         </button>
       </div>
